c92e389f0cd1a6edfd1bbe72bce4f3aa6072ddf6
Spandan Roy <royspandan1@gmail.com>
2024-12-12 13:50:20 +0530

    Buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,19 @@
 		<time-cards 
 			:cardData="cards"
 		/>
+
+		<!-- Floating Action Buttons -->
+		<div class="floating-buttons">
+			<button class="fab" @click="openModal('nextBlock')" title="Next block">
+				<chevron-right-icon />
+			</button>
+			<button class="fab" @click="openModal('changeBlock')" title="Change current block">
+				<swap-horizontal-icon />
+			</button>
+			<button class="fab" @click="openModal('addBlockType')" title="Add blocktype">
+				<plus-box-icon />
+			</button>
+		</div>
 	</div>
 </template>
 
@@ -18,11 +31,20 @@ import TimeCards from '../components/TimeCards.vue';
 import SemiClock from '../components/SemiClock.vue';
 import Status from '../components/Status.vue';
 
+import ChevronRightIcon from "vue-material-design-icons/ChevronRight.vue";
+import SwapHorizontalIcon from "vue-material-design-icons/SwapHorizontal.vue";
+import PlusBoxIcon from "vue-material-design-icons/PlusBox.vue";
+
+
 export default {
 	components: {
 		TimeCards,
 		SemiClock,
 		Status,
+
+		ChevronRightIcon,
+		SwapHorizontalIcon,
+		PlusBoxIcon
 	},
 	data() {
 		return {
@@ -47,6 +69,11 @@ export default {
 			}
 		};
 	},
+	methods: {
+		openModal(type: string) {
+			console.log(type);
+		}
+	}
 }
 </script>
 
@@ -59,5 +86,39 @@ export default {
 	color: #2c3e50;
 	background-color: #2e2e2e;
 	margin: 0;
+}
+
+.floating-buttons {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  z-index: 15;
+}
+
+.fab {
+  background-color: #6e006e;
+  color: white;
+  border: none;
+  border-radius: 25%;
+  width: 56px;
+  height: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  outline: none;
+}
+
+.fab:hover {
+  background-color: #3700b3;
+}
+
+.fab svg {
+  width: 24px;
+  height: 24px;
 }
 </style>
