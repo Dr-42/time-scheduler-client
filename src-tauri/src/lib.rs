19ff51e15bebd5ab49d83d17e5263dc8333af2a3
Spandan Roy <royspandan1@gmail.com>
2024-12-17 11:47:34 +0530

    User metadata tauface

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,10 @@ mod tauface;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![
+            tauface::save_meta,
+            tauface::get_meta
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/tauface.rs
@@ -0,0 +1,54 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Meta {
+    username: String,
+    pass_hash: String,
+    server_ip: String,
+}
+
+#[tauri::command]
+pub async fn save_meta(
+    username: Option<&str>,
+    pass_hash: Option<&str>,
+    server_ip: Option<&str>,
+) -> Result<(), String> {
+    let dirs = directories::ProjectDirs::from("org", "dr42", "timescheduler")
+        .ok_or("Directories doesn't work")?;
+    let cache_dir = dirs.cache_dir().to_owned();
+    let meta_path = cache_dir.join("meta.json");
+
+    let loaded_meta = match std::fs::read_to_string(&meta_path) {
+        Ok(meta) => {
+            let meta: Meta = serde_json::from_str(&meta).unwrap();
+            meta
+        }
+        Err(_) => Meta {
+            username: "".to_string(),
+            pass_hash: "".to_string(),
+            server_ip: "".to_string(),
+        },
+    };
+
+    let meta = Meta {
+        username: username.unwrap_or(&loaded_meta.username).to_string(),
+        pass_hash: pass_hash.unwrap_or(&loaded_meta.pass_hash).to_string(),
+        server_ip: server_ip.unwrap_or(&loaded_meta.server_ip).to_string(),
+    };
+
+    let meta_json = serde_json::to_string(&meta).unwrap();
+    std::fs::write(meta_path, meta_json).unwrap();
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn get_meta() -> Meta {
+    let dirs = directories::ProjectDirs::from("org", "dr42", "timescheduler")
+        .ok_or("Directories doesn't work")
+        .unwrap();
+    let cache_dir = dirs.cache_dir().to_owned();
+    let meta_path = cache_dir.join("meta.json");
+    let meta_json = std::fs::read_to_string(&meta_path).unwrap();
+    let meta: Meta = serde_json::from_str(&meta_json).unwrap();
+    meta
+}
